e42d25d6f7067789ea83a92e2a174a0917284868
Kyle Edwards <wingedasterisk@gmail.com>
2022-03-19 18:22:36 -0700

    preload: add app version to page toolbar, expose memory usage function from node process

--- assets/scripts/preload.js
+++ assets/scripts/preload.js
@@ -3,4 +3,7 @@ const pkg = require(`../../package.json`);
 
 window.addEventListener(`DOMContentLoaded`, () => {
   document.title = `Node Studio ${pkg.version}`;
+  document.querySelector(`#version`).innerHTML = document.title;
 });
+
+contextBridge.exposeInMainWorld(`memUsage`, process.memoryUsage);
