51cf63f5c2c4b7a942285877c60fc0d26ec0ec1c
Kyle Edwards <wingedasterisk@gmail.com>
2022-07-08 20:41:29 -0700

    open URLs in external browser

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const path = require(`path`);
-const { app, BrowserWindow, nativeImage } = require(`electron`);
+const { app, BrowserWindow, nativeImage, shell } = require(`electron`);
 
-// should fix color reproduction
+// fix color reproduction
 app.commandLine.appendSwitch(`force-color-profile`, `srgb`);
 
 app.on(`ready`, () => {
+  // create splash screen
+  // may be used later if initial loading times get long
+  // but for now, the editor loads almost instantly
+  // so this is unnecessary atm
+
+  // const splash = new BrowserWindow({
+  //   width: 600,
+  //   minWidth: 600,
+  //   maxWidth: 600,
+  //   height: 300,
+  //   minHeight: 300,
+  //   maxHeight: 300,
+  //   resizable: false,
+  //   frame: false
+  // });
+
+  // splash.loadFile(`assets/splash.html`);
+  // splash.center();
+
   const window = new BrowserWindow({
     width: 1000,
     height: 800,
@@ -18,13 +37,22 @@ app.on(`ready`, () => {
       enableRemoteModule: false,
       nodeIntegration: false // this should be false by default, but better safe than sorry
     },
-    icon: nativeImage.createFromPath(path.join(__dirname, `./assets/images/node1.png`))
+    icon: nativeImage.createFromPath(path.join(__dirname, `./assets/images/node-studio.ico`))
   });
-
+  
   window.loadFile(`assets/editor.html`);
 
   window.once(`ready-to-show`, () => {
     window.show();
+    // splash.destroy();
+
+    // makes web URLs open in the user's actual browser
+    window.webContents.on(`will-navigate`, (e, url) => {
+      e.preventDefault();
+      // rly basic, quick check to ensure we have no weird URLs
+      if (!(/^https?:/i).test(url)) return;
+      shell.openExternal(url);
+    });
   });
 });
 
