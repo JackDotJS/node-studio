59a3f96d480ff7d553c4404af50176c08601dc72
Kyle Edwards <wingedasterisk@gmail.com>
2022-07-09 22:20:47 -0700

    slider default values, even more layout and style changes

--- assets/scripts/slider.mjs
+++ assets/scripts/slider.mjs
@@ -9,7 +9,7 @@ export function loadSliders() {
     let active = false;
 
     // im going to fucking explode
-    const updatePos = (x, y) => {
+    const updatePos = (x, y, val) => {
       const elemBox = slider.getBoundingClientRect();
 
       const thumbsize = (vertical) ? thumb.offsetHeight : thumb.offsetWidth;
@@ -20,49 +20,46 @@ export function loadSliders() {
       const negRange = Math.abs(sliderData.min);
       const flipSignage = parseInt(sliderData.max) < parseInt(sliderData.min);
 
-      // get target position
       let targetPos;
-      if (vertical) {
-        targetPos = Math.max((thumbsize / 2), Math.min(sliderSize - (thumbsize / 2), y - elemBox.top));
-      } else {
-        targetPos = Math.max((thumbsize / 2), Math.min(sliderSize - (thumbsize / 2), x - elemBox.left));
-      }
+      let rawValue;
 
-      // get slider value from that position
-      const rawValue = Math.round(fullRange * ((targetPos - (thumbsize / 2)) / (sliderSize - (thumbsize))));
+      if (val) {
+        rawValue = Math.max(negRange, Math.min(fullRange, val));
+      } else {
+        // get target position
+        if (vertical) {
+          targetPos = Math.max((thumbsize / 2), Math.min(sliderSize - (thumbsize / 2), elemBox.bottom - y));
+        } else {
+          targetPos = Math.max((thumbsize / 2), Math.min(sliderSize - (thumbsize / 2), x - elemBox.left));
+        }
+  
+        // get slider value from that position
+        rawValue = Math.round(fullRange * ((targetPos - (thumbsize / 2)) / (sliderSize - (thumbsize))));
+      }
 
       // snap thumb to nearest position based on max value of the input range
       const thumbPos = Math.max(0, Math.min(100, ((rawValue / fullRange) * 100)));
 
       let newValue = rawValue - negRange;
       
-      // done!
       if (vertical) {
-        // "invert" value if it's a vertical slider
-        // because otherwise setting the slider to the lowest position
-        // would make it it's max value, which would be weird and counter-intuitive
-        newValue = ((1 - (rawValue / fullRange)) * fullRange) - negRange;
-
-        thumb.style.top = thumbPos + `%`;
+        thumb.style.bottom = thumbPos + `%`;
       } else {
         thumb.style.left = thumbPos + `%`;
       }
 
       if (flipSignage) newValue = -newValue;
 
-      sliderData.value = Math.round(newValue);
+      // stop here if the value is the same
+      // prevents infinite loop: DO NOT REMOVE!!!
+      // there's probably a better way to do this
+      if (sliderData.value == Math.round(newValue)) return;
+
+      thumb.setAttribute(`data-value`, Math.round(newValue));
     };
 
     //set initial thumb position
-    const initBox = slider.getBoundingClientRect();
-
-    if (vertical) {
-      const val = initBox.top + ((slider.offsetHeight / sliderData.max) * sliderData.value);
-      updatePos(0, val);
-    } else {
-      const val = initBox.left + ((slider.offsetWidth / sliderData.max) * sliderData.value);
-      updatePos(val, 0);
-    }
+    updatePos(null, null, sliderData.default);
 
     slider.addEventListener(`mousedown`, (e) => {
       if (e.button !== 0) return;
@@ -78,6 +75,13 @@ export function loadSliders() {
     window.addEventListener(`mouseup`, (e) => {
       if (e.button !== 0 || !active) return;
       active = false;
+    });
+
+    new MutationObserver((changes) => {
+      updatePos(null, null, changes[0].target.dataset.value);
+    }).observe(thumb, {
+      attributes: true,
+      attributeFilter: [`data-value`]
     });
   });
 }
