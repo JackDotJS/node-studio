1e47b9eecefc3f2f17e919e1ed78adedd1b2be46
Kyle Edwards <wingedasterisk@gmail.com>
2022-07-12 14:11:26 -0700

    blur textboxes on change, set window title function

--- assets/preload.js
+++ assets/preload.js
@@ -1,10 +1,15 @@
 const { contextBridge, ipcRenderer } = require(`electron`);
 const pkg = require(`../package.json`);
 
+const appVersion = `Node Studio ${pkg.version}`;
+
+function setWinTitle(text) {
+  document.title = `${text} | ${appVersion}`;
+}
+
 window.addEventListener(`DOMContentLoaded`, () => {
-  const title = `Node Studio ${pkg.version}`
-  document.title = `Untitled Project | ${title}`;
-  document.querySelector(`#version`).innerHTML = title;
+  document.querySelector(`#version`).innerHTML = appVersion;
 });
 
 contextBridge.exposeInMainWorld(`memUsage`, process.memoryUsage);
+contextBridge.exposeInMainWorld(`setWinTitle`, setWinTitle);
--- assets/scripts/core.mjs
+++ assets/scripts/core.mjs
@@ -1,28 +1,39 @@
 import { memory } from "./memory.mjs";
 
-document.addEventListener(`DOMContentLoaded`, () => {
-  memory.masterVolume = memory.audioCTX.createGain();
-  memory.masterAnalyser = memory.audioCTX.createAnalyser();
+memory.masterVolume = memory.audioCTX.createGain();
+memory.masterAnalyser = memory.audioCTX.createAnalyser();
 
-  memory.masterVolume.connect(memory.masterAnalyser);
-  memory.masterAnalyser.connect(memory.audioCTX.destination);
+memory.masterVolume.connect(memory.masterAnalyser);
+memory.masterAnalyser.connect(memory.audioCTX.destination);
 
-  memory.masterVolume.gain.value = 0.00;
+memory.masterVolume.gain.value = 0.00;
 
-  memory.masterAnalyser.fftSize = 256; // WARN: frequencyBinCount will always be half of this value!!!
-  memory.masterAnalyser.smoothingTimeConstant = 0.0;
+memory.masterAnalyser.fftSize = 256; // WARN: frequencyBinCount will always be half of this value!!!
+memory.masterAnalyser.smoothingTimeConstant = 0.0;
 
-  memory.willChange = 3458937459;
+memory.willChange = 3458937459;
 
-  setInterval(() => {
-    document.querySelector(`#latency`).innerHTML = `LAT: ${memory.audioCTX.baseLatency}s`;
-  }, 2500);
-  
-  import(`./graphs.mjs`);
-  import(`./piano.mjs`);
-  import(`./mem_usage.mjs`);
-  import(`./slider.mjs`);
-  import(`./context.mjs`);
-  import(`./classes/project.mjs`);
-  import(`./node_editor.mjs`);
-});
+setInterval(() => {
+  document.querySelector(`#latency`).innerHTML = `LAT: ${memory.audioCTX.baseLatency}s`;
+}, 2500);
+
+const pTitle = document.querySelector(`#projectTitle`);
+
+window.setWinTitle(pTitle.value || pTitle.placeholder);
+
+import(`./graphs.mjs`);
+import(`./piano.mjs`);
+import(`./mem_usage.mjs`);
+import(`./slider.mjs`);
+import(`./context.mjs`);
+import(`./classes/project.mjs`);
+import(`./node_editor.mjs`);
+
+document.addEventListener(`change`, (e) => {
+  // just to be sure
+  if (e.target.type !== `text` && e.target.type !== `number`) return;
+
+  if (e.target.id === `projectTitle`) window.setWinTitle(e.target.value || e.target.placeholder);
+
+  e.target.blur();
+}, true);
