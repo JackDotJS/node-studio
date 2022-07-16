1ee2887f09e58ce21e1e80d5b8713437efaed8b1
Kyle Edwards <wingedasterisk@gmail.com>
2022-07-16 04:32:21 -0700

    add util to help find specific datablocks in arrays

--- assets/scripts/classes/datablock.mjs
+++ assets/scripts/classes/datablock.mjs
@@ -3,3 +3,11 @@ export class DataBlock {
     this.id = id || window.crypto.randomUUID();
   }
 }
+
+export function indexInArray(arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) return i;
+  }
+
+  return -1;
+}
