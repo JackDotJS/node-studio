096a80ecd03f0b1dad7735245b11a07baa083a32
akii0008 <samir.js.zip@gmail.com>
2022-07-18 22:23:57 -0400

    updated typings: memUsage

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,3 @@
-declare const memUsage: NodeJS.MemoryUsage;
+declare const memUsage: () => NodeJS.MemoryUsage;
 declare const setProjectTitle: (text: string) => void;
 declare const DRPCResetTime: () => void;
