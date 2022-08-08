b4c01802a88af1605c866dd59105de17f68c732d
akii0008 <samir.js.zip@gmail.com>
2022-08-08 10:20:57 -0400

    moved typings to web folder since they are only used to declare globals in the *web* context

--- src/web/index.d.ts
+++ src/web/index.d.ts
@@ -2,4 +2,4 @@ declare const memUsage: () => NodeJS.MemoryUsage;
 declare const setProjectTitle: (text: string) => void;
 declare const DRPCResetTime: () => void;
 
-declare function isInterface<T>(obj: any, key: keyof T): obj is T; 
+declare function isInterface<T>(obj: any, key: keyof T): obj is T;
