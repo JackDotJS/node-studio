4169a086e124cc14b5196972550f7e0231aaff45
akii0008 <samir.js.zip@gmail.com>
2022-12-16 21:43:29 -0500

    fix setting the window title

--- src/util/set-project-title.ts
+++ src/util/set-project-title.ts
@@ -1,7 +1,13 @@
 import { getName, getVersion } from "@tauri-apps/api/app";
+import { appWindow } from "@tauri-apps/api/window";
 
-export default function setProjectTitle(text: string) {
-  document.title = `${text} | ${getName()} ${getVersion()}`;
+export default async function setProjectTitle(text: string) {
+  const appName = await getName();
+  const appVersion = await getVersion();
+  
+  document.title = `${text} | ${appName} ${appVersion}`;
+  appWindow.setTitle(`${text} | ${appName} ${appVersion}`);
+
   // todo: reimplement discord rpc
   //drpc.setDetails(text);
 }
