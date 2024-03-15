12cebe54c94ac7f51215901e9530115b084ac93e
bdotsamir <samir.js.zip@gmail.com>
2024-03-15 17:48:48 -0400

    Header: add disabled spectrum analyser

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import styles from "../css/Header.module.css";
+// import SpectrumGraph from "./SpectrumGraph";
 import WaveformGraph from "./WaveformGraph";
 
 export default function Header() {
   return (
-    <div class={styles.bar}>
+    <div class={styles.header}>
       <input type="button" value="File" />
       <input type="button" value="Edit" />
       <input type="button" value="View" />
@@ -11,10 +12,16 @@ export default function Header() {
       <input type="button" value="Track" />
       <input type="button" value="Help" />
 
-      <input type="text" placeholder="Untitled Project" />
+      <input
+        style={{ flex: 1 }}
+        type="text"
+        placeholder="Untitled Project"
+      />
 
       <WaveformGraph />
-      
+      {/* <SpectrumGraph /> 
+      // FIXME: large box appearing in place of spectrum graph */}
+
     </div>
   )
 }
---
+++ src/components/SpectrumGraph.tsx
@@ -0,0 +1,64 @@
+import { onMount, useContext } from "solid-js";
+import { MemoryContext } from "../MemoryContext";
+
+export default function SpectrumGraph() {
+  let spectrumRef!: HTMLCanvasElement;
+
+  const ctx = useContext(MemoryContext);
+  if (!ctx?.masterAnalyser) throw new Error("Missing context master analyser");
+
+  const size = ctx.masterAnalyser.fftSize;
+
+  function drawSpectrumGraph() {
+    if (!ctx?.masterAnalyser) throw new Error("drawSpectrumGraph: Missing context master analyser");
+
+    const spectrumCTX = spectrumRef.getContext("2d");
+    if (!spectrumCTX) throw new Error("Spectrum canvas does not have a 2d context");
+
+    // fix stupid canvas size bullshittery
+    spectrumCTX.canvas.width = spectrumRef.offsetWidth;
+    spectrumCTX.canvas.height = spectrumRef.offsetHeight;
+
+    spectrumCTX.clearRect(0, 0, spectrumRef.width, spectrumRef.height);
+
+    const data = new Float32Array(size);
+    // const data = new Uint8Array(size);
+    ctx?.masterAnalyser?.getFloatFrequencyData(data);
+    //memory.masterAnalyser.getByteFrequencyData(data);
+
+    const width = spectrumRef.width / data.length;
+
+    let x = 0;
+
+    for (let i = 0; i < (spectrumRef.width / width); i++) {
+      // values for float data (i want to cry)
+      const dbValue = data[Math.round(i * data.length / (spectrumRef.offsetWidth / width))];
+      const mappedDb = (dbValue - (ctx.masterAnalyser.minDecibels)) * (spectrumRef.offsetHeight / 2) / (ctx.masterAnalyser.maxDecibels - ctx.masterAnalyser.minDecibels) + (spectrumRef.height / 2);
+
+      // values for byte data
+      // const dbValue = data[Math.round(i * data.length / (spectrumRef.offsetWidth / width))];
+      // const mappedDb = dbValue * (spectrumRef.offsetHeight / 2) / 255;
+
+      const height = Math.max(1, mappedDb);
+
+      spectrumCTX.fillStyle = getComputedStyle(spectrumRef).getPropertyValue(`color`);
+
+      spectrumCTX.fillRect(
+        x,
+        (spectrumRef.height / 2) - Math.max(1, height / 2),
+        width,
+        height,
+      );
+
+      x += width;
+    }
+
+    requestAnimationFrame(drawSpectrumGraph);
+  }
+
+  onMount(() => drawSpectrumGraph());
+
+  return (
+    <canvas ref={spectrumRef} />
+  )
+}
