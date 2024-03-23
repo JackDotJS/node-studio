6c246931a677bbdd3233ccde618db050a4e6ed7b
bdotsamir <samir.js.zip@gmail.com>
2024-03-23 15:51:09 -0400

    change from input type button to just button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import styles from "../css/Header.module.css";
-// import SpectrumGraph from "./SpectrumGraph";
+import SpectrumGraph from "./SpectrumGraph";
 import WaveformGraph from "./WaveformGraph";
 
 export default function Header() {
   return (
     <div class={styles.header}>
-      <input type="button" value="File" />
-      <input type="button" value="Edit" />
-      <input type="button" value="View" />
-      <input type="button" value="Nodes" />
-      <input type="button" value="Track" />
-      <input type="button" value="Help" />
+      <button>File</button>
+      <button>Edit</button>
+      <button>View</button>
+      <button>Nodes</button>
+      <button>Track</button>
+      <button>Help</button>
 
       <input
         style={{ flex: 1 }}
@@ -19,8 +19,7 @@ export default function Header() {
       />
 
       <WaveformGraph />
-      {/* <SpectrumGraph /> 
-      // FIXME: large box appearing in place of spectrum graph */}
+      <SpectrumGraph />
 
     </div>
   )
