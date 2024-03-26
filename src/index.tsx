44c17a8690d50a7d24dd0b061643bb16ce879e77
bdotsamir <samir.js.zip@gmail.com>
2024-03-26 19:30:38 -0400

    index: attach app to document.body

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,4 @@ import { render } from 'solid-js/web';
 
 import App from './app';
 
-const root = document.getElementById('root');
-
-if (!(root instanceof HTMLElement)) {
-  throw new Error(`Root element not found.`);
-}
-
-render(() => <App />, root);
+render(() => <App />, document.body);
