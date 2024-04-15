7f2e1a45b09f321cad0f2ec73ad45261e78bdfc9
bdotsamir <samir.js.zip@gmail.com>
2024-04-14 22:38:58 -0400

    DELETE OLD FOLDER

--- src/old/util/get-memory-usage.ts
+++
@@ -1,7 +0,0 @@
-import { getName, getVersion } from "@tauri-apps/api/app";
-
-export default function setProjectTitle(text: string) {
-  document.title = `${text} | ${getName()} ${getVersion()}`;
-  // todo: reimplement discord rpc
-  //drpc.setDetails(text);
-}
