94bc1fe1636cf10f82518b5f8303da3ce1f0991d
bdotsamir <samir.js.zip@gmail.com>
2024-04-15 20:37:59 -0400

    Splitable: start work on context
    Co-authored-by: JackDotJS <jackdotbusiness@proton.me>

--- src/components/Splitable.tsx
+++ src/components/Splitable.tsx
@@ -1,5 +1,20 @@
-import { JSX, Match, Switch } from "solid-js";
+import { JSX, Match, Switch, createContext, createSignal, useContext } from "solid-js";
 import SplitLine from "./SplitLine";
+
+export const SplitableContext = createContext<{
+  elementOne: HTMLDivElement,
+  elementTwo: HTMLDivElement,
+
+  setElementOne: (element: HTMLDivElement) => void, // eslint-disable-line no-unused-vars
+  setElementTwo: (element: HTMLDivElement) => void, // eslint-disable-line no-unused-vars
+}>();
+
+export function useSplitableContext() {
+  const context = useContext(SplitableContext);
+  if (!context) throw new Error(`useSplitableContext: SplitableContext is undefined!`);
+
+  return context;
+}
 
 type SplitableProps = {
   type: "horizontal" | "vertical",
@@ -12,23 +27,34 @@ type SplitableProps = {
  * @returns {JSX.Element}
  */
 export default function Splitable(props: SplitableProps) {
+
+  const [elementOne, setElementOne] = createSignal<HTMLDivElement>();
+  const [elementTwo, setElementTwo] = createSignal<HTMLDivElement>();
+
   return (
-    <Switch fallback={<FallbackComponent />}>
-      <Match when={props.type === "vertical"}>
-        <div style={{ display: "flex", "flex-direction": "column", flex: 1 }}>
-          {props.children[0]}
-          <SplitLine type="vertical" />
-          {props.children[1]}
-        </div>
-      </Match>
-      <Match when={props.type === "horizontal"}>
-        <div style={{ display: "flex", "flex-direction": "row", flex: 1 }}>
-          {props.children[0]}
-          <SplitLine type="horizontal" />
-          {props.children[1]}
-        </div>
-      </Match>
-    </Switch>
+    <SplitableContext.Provider value={{
+      get elementOne() { return elementOne()!; },
+      get elementTwo() { return elementTwo()!; },
+      setElementOne,
+      setElementTwo
+    }}>
+      <Switch fallback={<FallbackComponent />}>
+        <Match when={props.type === "vertical"}>
+          <div style={{ display: "flex", "flex-direction": "column", flex: 1 }}>
+            {props.children[0]}
+            <SplitLine type="vertical" />
+            {props.children[1]}
+          </div>
+        </Match>
+        <Match when={props.type === "horizontal"}>
+          <div style={{ display: "flex", "flex-direction": "row", flex: 1 }}>
+            {props.children[0]}
+            <SplitLine type="horizontal" />
+            {props.children[1]}
+          </div>
+        </Match>
+      </Switch>
+    </SplitableContext.Provider>
   );
 }
 
